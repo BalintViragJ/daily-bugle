9eb1bc7e8ffc8c4d44f0c8cfa1e92932230f9e4e
Konya <edit.konya83@gmail.com>
2021-03-02 12:34:51 +0100

    JournalistRepository 1.0

---
+++ src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/repository/JournalistRepository.java
@@ -0,0 +1,33 @@
+package hu.progmasters.ujratervezes.week16.dailybugle.repository;
+
+import hu.progmasters.ujratervezes.week16.dailybugle.domain.Journalist;
+import hu.progmasters.ujratervezes.week16.dailybugle.dto.JournalistCreateData;
+
+import java.util.List;
+
+public class JournalistRepository {
+    public List<Journalist> getJournalists() {
+
+        return null;
+    }
+
+    public Journalist findJournalist(int id) {
+
+        return null;
+    }
+
+    public boolean createJournalist(JournalistCreateData data) {
+
+        return false;
+    }
+
+    public boolean updateJournalist(int id, JournalistCreateData data) {
+
+        return false;
+    }
+
+    public boolean deleteJournalist(int id) {
+
+        return false;
+    }
+}
--- src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/service/JournalistService.java
+++ src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/service/JournalistService.java
@@ -3,29 +3,53 @@ package hu.progmasters.ujratervezes.week16.dailybugle.service;
 
 import hu.progmasters.ujratervezes.week16.dailybugle.domain.Journalist;
 import hu.progmasters.ujratervezes.week16.dailybugle.dto.JournalistCreateData;
+import hu.progmasters.ujratervezes.week16.dailybugle.repository.JournalistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Service
+@Transactional
 public class JournalistService {
 
+    private JournalistRepository journalistRepository;
+
+    @Autowired
+    public JournalistService(JournalistRepository journalistRepository) {
+        this.journalistRepository = journalistRepository;
+    }
 
     public List<Journalist> getJournalists() {
+        List<Journalist> journalists = journalistRepository.getJournalists();
+
         return null;
     }
 
     public Journalist findJournalist(int id) {
+        Journalist journalist = journalistRepository.findJournalist(id);
+
         return null;
     }
 
     public boolean createJournalist(JournalistCreateData data) {
+        boolean saveSuccessful = journalistRepository.createJournalist(data);
+
         return false;
     }
 
     public boolean updateJournalist(int id, JournalistCreateData data) {
+        boolean updateSuccessful = journalistRepository.updateJournalist(id, data);
+
         return false;
     }
 
     public boolean deleteJournalist(int id) {
+        boolean deleteSuccessful = journalistRepository.deleteJournalist(id);
+
+
         return false;
     }
 }

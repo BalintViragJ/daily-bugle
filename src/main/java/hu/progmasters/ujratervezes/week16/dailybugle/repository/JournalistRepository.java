75eb3d413f6edb9f7acacd3d1513729b782d3a0e
Konya <edit.konya83@gmail.com>
2021-03-02 13:02:20 +0100

    JournalistRepository get+find 1.0

--- src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/repository/JournalistRepository.java
+++ src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/repository/JournalistRepository.java
@@ -2,18 +2,50 @@ package hu.progmasters.ujratervezes.week16.dailybugle.repository;
 
 import hu.progmasters.ujratervezes.week16.dailybugle.domain.Journalist;
 import hu.progmasters.ujratervezes.week16.dailybugle.dto.JournalistCreateData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class JournalistRepository {
-    public List<Journalist> getJournalists() {
 
-        return null;
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JournalistRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Journalist> getJournalists() {
+        String sql = "SELECT * FROM journalist";
+        return jdbcTemplate.query(sql, (resultSet, rowNumber) -> {
+            Journalist journalist = new Journalist();
+            journalist.setName(resultSet.getString("name"));
+            journalist.setAddress(resultSet.getString("address"));
+            journalist.setEmail(resultSet.getString("email"));
+            journalist.setTelephoneNumber(resultSet.getInt("telephoneNumber"));
+            return journalist;
+        });
     }
 
     public Journalist findJournalist(int id) {
-
-        return null;
+        try {
+        String sql = "SELECT * FROM journalist WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, (resultSet, rowNumber) -> {
+            Journalist journalist = new Journalist();
+            journalist.setName(resultSet.getString("name"));
+            journalist.setAddress(resultSet.getString("address"));
+            journalist.setEmail(resultSet.getString("email"));
+            journalist.setTelephoneNumber(resultSet.getInt("telephoneNumber"));
+            return journalist;
+        });
+        }catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     public boolean createJournalist(JournalistCreateData data) {

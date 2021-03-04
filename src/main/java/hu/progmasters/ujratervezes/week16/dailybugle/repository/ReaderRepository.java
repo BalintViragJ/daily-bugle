aaad46b21484be9346dd869b7316492451542252
TRuvemon <truvemon@gmail.com>
2021-03-04 11:29:44 +0100

    @Repository fixed

--- src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/repository/ReaderRepository.java
+++ src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/repository/ReaderRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
+@Repository
 public class ReaderRepository {
 private final JdbcTemplate jdbcTemplate;
 
